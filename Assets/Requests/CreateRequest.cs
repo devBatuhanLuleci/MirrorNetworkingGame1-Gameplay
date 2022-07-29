ef5a57fdebbdbd84cd1cde7f134fbe38be1262a1
Exop63 <abdulahbaglan@gmail.com>
2022-07-29 16:03:39 +0300

    init

---
+++ Assets/Requests/CreateRequest.cs
@@ -0,0 +1,31 @@
+﻿using Assets.HttpClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CreateRequest : IPostRequest
+{
+    public string Url => "Auth/create";
+
+    public string moralisId;
+    public string email;
+
+    public CreateRequest(string moralisId, string email)
+    {
+        this.moralisId = moralisId;
+        this.email = email;
+    }
+
+    public WWWForm ToForm()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("moralisId", moralisId);
+        form.AddField("email", email);
+
+        return form;
+    }
+}
+
