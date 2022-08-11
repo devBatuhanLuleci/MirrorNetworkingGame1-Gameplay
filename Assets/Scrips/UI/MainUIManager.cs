1ddac2700d8993ce5a98e1559e249e9f41a97faa
Exop63 <abdullah@sourdough.com.tr>
2022-08-11 15:16:52 +0300

    Panel class added

---
+++ Assets/Scrips/UI/MainUIManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIManager : Singleton<MainUIManager>
+{
+    [Header("Setup")]
+    [SerializeField]
+    private Panel LoadingPanel;
+}
