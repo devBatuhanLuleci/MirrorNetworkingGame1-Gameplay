7228c9a24de7a74117cfc86c9b8cb95d177c52c6
devBatuhanLuleci <batuhan@anycivilization.com>
2023-04-10 18:02:49 +0300

    Crystal canvas revizedl vl3.

--- Assets/CrystalStartInfoPanel.cs
+++ Assets/CrystalStartInfoPanel.cs
@@ -22,23 +22,56 @@ public class CrystalStartInfoPanel : Panel
         // m_MyFirstAction.AddListener(m_MyFirstAction);
         // CrystalInfoRectTransform.DOScale(1f, 1f).From(0f).SetLoops(2,LoopType.Yoyo).SetEase(Ease.OutQuad).SetDelay(2);
 
-        HandleScaleOfCrystalInfoRectTransform();
+      StartCoroutine(HandleScaleOfCrystalInfoRectTransformCoroutine());
     }
-    public void HandleScaleOfCrystalInfoRectTransform()
+    public IEnumerator HandleScaleOfCrystalInfoRectTransformCoroutine()
     {
+        float elapsedTime = 0f;
+        float duration = 1.5f;
+
+        // Scale up
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            float scale = Mathf.Lerp(0f, 1f, t);
+            CrystalInfoRectTransform.localScale = new Vector3(scale, scale, 1f);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        CrystalInfoRectTransform.localScale = Vector3.one;
+
+        // Wait for 1 second
+        yield return new WaitForSeconds(1f);
+
+        // Scale down
+        elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            float scale = Mathf.Lerp(1f, 0f, t);
+            CrystalInfoRectTransform.localScale = new Vector3(scale, scale, 1f);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        CrystalInfoRectTransform.localScale = Vector3.zero;
 
 
-        Sequence s = DOTween.Sequence();
-        s.Append(CrystalInfoRectTransform.DOScale(1f, 1.5f).From(0f).SetEase(Ease.OutBack));
-        s.AppendInterval(1f);
-        s.Append(CrystalInfoRectTransform.DOScale(0f, 1.5f).SetEase(Ease.InBack));
-        s.SetDelay(2f);
-
-        s.OnComplete(() => {
-            // Debug.Log("Animasyon tamamlandı.");
-            OnAnimationFinished();
-        });
+        OnAnimationFinished();
     }
+    //public void SequenceRectTransformScale()
+    //{
+    //    Sequence s = DOTween.Sequence();
+    //    s.Append(CrystalInfoRectTransform.DOScale(1f, 1.5f).From(0f).SetEase(Ease.OutBack));
+    //    s.AppendInterval(1f);
+    //    s.Append(CrystalInfoRectTransform.DOScale(0f, 1.5f).SetEase(Ease.InBack));
+    //    s.SetDelay(2f);
+     
+    //    s.OnComplete(() => {
+    //        // Debug.Log("Animasyon tamamlandı.");
+    //        OnAnimationFinished();
+    //    });
+    //}
+
     private void OnAnimationFinished()
     {
         CloseSmoothly();
